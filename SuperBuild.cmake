0b17625e5e38e8d6cae707e1e572b1e407860b91
Arthur Le Maout <arthur.le-maout@cpe.fr>
2018-10-19 09:56:28 -0400

    COMP: Fixed source files compilation

--- SuperBuild.cmake
+++ SuperBuild.cmake
@@ -140,4 +140,22 @@ if(verbose)
   endforeach()
 endif()
 
+#Qt example------------------------------------
+
+set(CMAKE_INCLUDE_CURRENT_DIR ON)
+set(CMAKE_AUTOMOC ON)
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY  ${CMAKE_CURRENT_BINARY_DIR}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY  ${CMAKE_CURRENT_BINARY_DIR}/lib)
+
+# Find the QtWidgets library
+
+# find Qt5 headers
+find_package(Qt5 COMPONENTS Widgets REQUIRED)
+
+include_directories(${Qt5Widgets_INCLUDE_DIRS})
+add_definitions(${Qt5Widgets_DEFINITIONS})
+
+set(QT_LIBRARIES ${Qt5Widgets_LIBRARIES})
+
 add_subdirectory(src)
