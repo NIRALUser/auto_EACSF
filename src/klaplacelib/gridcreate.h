d42dcbdc7d58137add9295850beae8e30ead8934
Arthur Le Maout <arthur.le-maout@cpe.fr>
2019-01-18 15:49:24 -0500

    STYLE: struct GridCreate replaced by new separate eponym class

--- src/klaplacelib/gridcreate.h
+++ src/klaplacelib/gridcreate.h
@@ -1,11 +1,24 @@
 #ifndef GRIDCREATE_H
 #define GRIDCREATE_H
 
+#include <vtkImageData.h>
+#include <vtkPolyData.h>
+#include <vtkStructuredGrid.h>
 
-class GridCreate
-{
+// compute the common voxel space
+class GridCreate{
 public:
-    GridCreate();
+    GridCreate(double* bounds, const int dims);
+
+    void createImage(vtkImageData* im);
+
+    vtkStructuredGrid* createStencil(vtkImageData* im, vtkPolyData* surf);
+
+    int dim[3];
+    double center[3];
+    double spacing[3];
+    int extent[6];
+    double origin[3];
 };
 
-#endif // GRIDCREATE_H
+#endif
--- src/klaplacelib/surfacecorrespondence.cxx
+++ src/klaplacelib/surfacecorrespondence.cxx
@@ -8,6 +8,7 @@ using namespace std;
 #include "surfacecorrespondence.h"
 #include "vtkio.h"
 #include "geometry.h"
+#include "gridcreate.h"
 
 #include <vtkCellData.h>
 #include <vtkCellDerivatives.h>
@@ -37,102 +38,10 @@ using namespace std;
 #include <vtkStructuredGrid.h>
 #include <vtkThresholdPoints.h>
 
-#include <vtkPolyDataMapper.h>
-#include <vtkDataSetMapper.h>
-#include <vtkImageMapper3D.h>
-#include <vtkActor.h>
-#include <vtkImageActor.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindowInteractor.h>
-
 #include <vtkImageWriter.h>
 #include <vtkMetaImageWriter.h>
 
 vtkDataSet* createGrid(vtkPolyData* osurf, vtkPolyData* isurf, const int dims, size_t& insideCountOut) {
-	
-	// compute the common voxel space
-	struct GridCreate {
-		int dim[3];
-		double center[3];
-		double spacing[3];
-		int extent[6];
-		double origin[3];
-		
-		GridCreate(double* bounds, const int dims) {
-			double maxbound = max(bounds[1]-bounds[0], max(bounds[3]-bounds[2], bounds[5]-bounds[4]));
-			
-			center[0] = (bounds[1]+bounds[0])/2.0;
-			center[1] = (bounds[3]+bounds[2])/2.0;
-			center[2] = (bounds[5]+bounds[4])/2.0;
-			
-			double gridSpacing = maxbound / dims;
-			spacing[0] = spacing[1] = spacing[2] = gridSpacing;
-			
-			dim[0] = (bounds[1]-bounds[0])/gridSpacing;
-			dim[1] = (bounds[3]-bounds[2])/gridSpacing;
-			dim[2] = (bounds[5]-bounds[4])/gridSpacing;
-			
-			extent[0] = extent[2] = extent[4] = 0;
-			extent[1] = dim[0] + 6;
-			extent[3] = dim[1] + 6;
-			extent[5] = dim[2] + 6;
-			
-			origin[0] = bounds[0] + gridSpacing*-3;
-			origin[1] = bounds[2] + gridSpacing*-3;
-			origin[2] = bounds[4] + gridSpacing*-3;
-			
-			cout << "Grid Dimension: " << dims << "; Grid Spacing: " << gridSpacing << " dim: " << dim[0] << "," << dim[1] << "," << dim[2] <<endl;
-		}
-		
-		
-		void createImage(vtkImageData* im) {
-			im->SetSpacing(spacing);
-			im->SetExtent(extent);
-			im->SetOrigin(origin);
-			im->AllocateScalars(VTK_INT, 1);
-			im->GetPointData()->GetScalars()->FillComponent(0, 255);
-		}
-		
-		vtkStructuredGrid* createStencil(vtkImageData* im, vtkPolyData* surf) {
-			createImage(im);
-
-			vtkNew<vtkPolyDataToImageStencil> psten;
-			psten->SetInputData(surf);
-			psten->SetOutputOrigin(origin);
-			psten->SetOutputSpacing(spacing);
-			psten->SetOutputWholeExtent(extent);
-			psten->Update();
-			
-			vtkNew<vtkImageStencil> isten;
-			isten->SetInputData(im);
-			isten->SetStencilData(psten->GetOutput());
-			isten->ReverseStencilOff();
-			isten->SetBackgroundValue(0);
-			isten->Update();
-
-			
-			vtkImageData* imgGrid = isten->GetOutput();
-			// visualiseDataSet(imgGrid);
-			vtkNew<vtkMetaImageWriter> imageWriter;
-			imageWriter->SetInputData(imgGrid);
-			imageWriter->SetFileName("testimage.mhd");
-			imageWriter->Write();
-			imgGrid->GetPointData()->GetScalars()->SetName("SampledValue");
-			
-			
-			vtkNew<vtkImageToStructuredGrid> imgToGrid;
-			imgToGrid->SetInputData(imgGrid);
-			imgToGrid->Update();
-			
-			vtkStructuredGrid* output = imgToGrid->GetOutput();
-			output->GetPointData()->SetScalars(imgGrid->GetPointData()->GetScalars());
-			output->Register(NULL);
-			
-			return output;
-		}
-	};
-	
 	GridCreate gc(osurf->GetBounds(), dims);
 	
 	vtkNew<vtkImageData> gim;
