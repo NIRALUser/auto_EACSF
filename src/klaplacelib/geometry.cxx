1392f2ff577a4947b9b07c4a17e54a5365ca3741
Arthur Le Maout <arthur.le-maout@cpe.fr>
2019-01-23 13:17:03 -0500

    STYLE: Extract neighbors function now static

--- src/klaplacelib/geometry.cxx
+++ src/klaplacelib/geometry.cxx
@@ -8,23 +8,27 @@
 
 using namespace std;
 
+Geometry::Geometry() {}
+
 size_t Geometry::extractNeighbors(vtkDataSet *ds, NeighborList &nbrs) {
-    const /*size_t*/vtkIdType nPoints = ds->GetNumberOfPoints();
+    const vtkIdType nPoints = ds->GetNumberOfPoints();
 	nbrs.resize(nPoints);
 	
 	vtkNew<vtkIdList> cellIds;
-    for (/*size_t*/int j = 0; j < nPoints; j++) {
+    for (vtkIdType j = 0; j < nPoints; j++) { // iterate through the points of the dataset
 		Neighbors& nbrPts = nbrs[j];
 		cellIds->Reset();
 		ds->GetPointCells(j, cellIds.GetPointer());
-        for (/*size_t*/int k = 0; k < cellIds->GetNumberOfIds(); k++) {
+        for (vtkIdType k = 0; k < cellIds->GetNumberOfIds(); k++) { // for each point, iterate through the cells using this point
 			vtkIdType cellId = cellIds->GetId(k);
 			vtkCell* cell = ds->GetCell(cellId);
-            for (/*size_t*/int l = 0; l < cell->GetNumberOfEdges(); l++) {
+            for (vtkIdType l = 0; l < cell->GetNumberOfEdges(); l++) { // for each cell, iterate through the edges of this cell
 				vtkCell* edge = cell->GetEdge(l);
 				vtkIdType s = edge->GetPointId(0);
 				vtkIdType e = edge->GetPointId(1);
-				if (s == j) {
+
+                // if one of the vertices in the edge is the starting point (j), add the other to its neighbor list if not there already
+                if (s == j) {
 					if (find(nbrPts.begin(), nbrPts.end(), e) == nbrPts.end()) {
 						nbrPts.push_back(e);
 					}
--- src/klaplacelib/geometry.h
+++ src/klaplacelib/geometry.h
@@ -11,32 +11,15 @@
 
 class Geometry {
 public:
-    vtkNew<vtkTransform> txfm;
-
-	typedef std::map<vtkIdType, Edge> EdgeMap;
+    typedef std::map<vtkIdType, Edge> EdgeMap;
     typedef std::vector<EdgeMap> EdgeList;
-	typedef std::vector<vtkIdType> Neighbors;
-	typedef std::vector<std::vector<vtkIdType> > NeighborList;
-	
-    // size_t extractEdges(vtkDataSet* ds, EdgeList& edges);
-	size_t extractNeighbors(vtkDataSet* ds, NeighborList& nbrs);
-    
-    /*
-    double tangentVector(const double u[3], const double v[3], const double n[3], double tv[3], vtkTransform* txf = NULL);
-	
-	double rotateVector(const double p[3], const double q[3], vtkTransform* txf, double* crossOut = NULL);
-	
-	double rotatePlane(const double u1[3], const double v1[3], const double u2[3], const double u3[3], vtkTransform* txf);
-    
-    double normalizeToNorthPole(const double u[3], const double n[3], double* cross, vtkTransform* txf);
-    
-    double denormalizeFromNorthPole(const double u[3], const double n[3], double* cross, vtkTransform* txf);
-	
-	
-	bool computeContourNormals(vtkDataSet* ds, vtkDoubleArray* normals);
-	
-	vtkPolyData* computeSurfaceNormals(vtkPolyData* pd);
-	
-	void print();
-	*/
+    typedef std::vector<vtkIdType> Neighbors;
+    typedef std::vector<std::vector<vtkIdType> > NeighborList;
+
+    Geometry();
+
+    static size_t extractNeighbors(vtkDataSet* ds, NeighborList& nbrs);
+
+private:
+    vtkNew<vtkTransform> txfm;
 };
