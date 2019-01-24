ee0f5e4dc82b16a91d4c772e642cfcd267aabcd8
Arthur Le Maout <arthur.le-maout@cpe.fr>
2019-01-24 11:20:34 -0500

    STYLE: Removing useless code from vtkIO class

--- src/klaplacelib/vtkio.cxx
+++ src/klaplacelib/vtkio.cxx
@@ -41,60 +41,7 @@ static bool endswith(std::string file, std::string ext) {
     return file.rfind(ext) == (unsigned int)epos;
 }
 
-//void vtkIO::zrotate(vtkPolyData* p) {
-//    int np = p->GetNumberOfPoints();
-//    vtkPoints* points = p->GetPoints();
-//    for (int i = 0; i < np; i++) {
-//        double x[3];
-//        points->GetPoint(i, x);
-////        cout << x[0] << "," << x[1] << "," << x[2] << endl;
-//        x[0] = -x[0];
-//        x[1] = -x[1];
-//        points->SetPoint(i, x);
-////        cout << x[0] << "," << x[1] << "," << x[2] << endl;
-//    }
-//    p->SetPoints(points);
-//}
-
-/*
-/// @param v1 a vector to rotate
-/// @param v2 a vector to which v1 rotates
-/// @param rotation an output matrix that stores a transform v1 to v2
-void vtkIO::rotateVector(const double *x1, const double *x2, vnl_matrix<double> &rotation) {
-    double v1[3], v2[3], vn[3];
-
-    /// Compute the norm for v1 and v2
-    const double n1 = vtkMath::Norm(x1);
-    const double n2 = vtkMath::Norm(x2);
-
-    for (int i = 0; i < 3; i++) {
-        v1[i] = x1[i] / n1;
-        v2[i] = x2[i] / n2;
-    }
-
-    /// Compute the cross product
-    vtkMath::Cross(v1, v2, vn);
-
-    double nn = vtkMath::Normalize(vn);
-    double theta = asin(nn);
-
-    /// sin(theta)
-    double st = nn;
-    /// 1 - cos(theta)
-    double cct = 1 - cos(theta);
-
-    rotation.set_size(3, 3);
-    rotation[0][0] = 1 + cct * (vn[0]*vn[0] - 1);
-    rotation[0][1] = 0 - vn[2] * st + cct * (vn[0]*vn[1]);
-    rotation[0][2] = 0 + vn[1] * st + cct * (vn[0]*vn[2]);
-    rotation[1][0] = 0 + vn[2] * st + cct * (vn[0]*vn[1]);
-    rotation[1][1] = 1 + cct * (vn[1]*vn[1] - 1);
-    rotation[1][2] = 0 - vn[0] * st + cct * (vn[1] * vn[2]);
-    rotation[2][0] = 0 - vn[1] * st + cct * (vn[0] * vn[2]);
-    rotation[2][1] = 0 + vn[0] * st + cct * (vn[1] * vn[2]);
-    rotation[2][2] = 1 + cct * (vn[2]*vn[2] - 1);
-}
-*/
+vtkIO::vtkIO(){}
 
 /// @brief Read a vtk/vtp file. The file's type is automatically determined by its extension.
 vtkPolyData* vtkIO::readFile(std::string file) {
@@ -118,26 +65,6 @@ vtkPolyData* vtkIO::readFile(std::string file) {
 	}
     return NULL;
 }
-
-
-//vtkDataArray* vtkIO::findFieldData(vtkPolyData* dataSet, std::string propertyName) {
-//    vtkDataArray* dataArray = NULL;
-//    int nArr = dataSet->GetFieldData()->GetNumberOfArrays();
-    
-//    dataSet->Print(cout);
-
-//    cout << "number of arrays found: " << nArr << endl;
-//    for (int j = 0; j < nArr; j++) {
-//        std::string arrayName(dataSet->GetFieldData()->GetArrayName(j));
-//        std::cout << "found " << arrayName << std::endl;
-//        if (propertyName == arrayName) {
-//            dataArray = dataSet->GetPointData()->GetArray(j);
-//            break;
-//        }
-//    }
-//    return dataArray;
-//}
-
 
 /// @brief Write a vtk/vtp file. The file's type is automatically determined by its extension.
 void vtkIO::writeFile(std::string file, vtkDataSet *mesh) {
@@ -178,36 +105,6 @@ void vtkIO::writeFile(std::string file, vtkDataSet *mesh) {
 	}
 	cout << "Write " << file << " done ..." << endl;
 }
-
-
-//void vtkIO::writeXMLFile(std::string file, vtkPolyData *mesh) {
-//    vtkXMLPolyDataWriter* w = vtkXMLPolyDataWriter::New();
-//    w->SetInputData(mesh);
-//    w->SetFileName(file.c_str());
-//    w->SetDataModeToAppended();
-//    w->EncodeAppendedDataOff();
-//    w->SetCompressorTypeToZLib();
-//    w->SetDataModeToBinary();
-//    w->Write();
-//    w->Delete();
-//}
-//
-//vtkDataArray* vtkIO::getPointArray(vtkDataSet* ds) {
-//    if (ds == NULL) {
-//        return NULL;
-//    }
-//
-//    vtkDataArray* da = NULL;
-//    for (int i = 0; i < ds->GetPointData()->GetNumberOfArrays(); i++) {
-//        ds->GetPointData()->GetArray(const char *arrayName)
-//    }
-//
-//    return da;
-//}
-
-
-
-
 
 vtkDataSet* vtkIO::readDataFile(std::string file) {
 	if (endswith(file, ".vtp")) {
--- src/klaplacelib/vtkio.h
+++ src/klaplacelib/vtkio.h
@@ -19,21 +19,10 @@ class vtkStructuredGrid;
 
 class vtkIO {
 public:
-    /// @brief Rotate the object around z-axis. Change the sign of x and y coordinates. This works in place.
-    //void zrotate(vtkPolyData* p);
-
-    /// @brief Create a rotation matrix that rotates v1 to v2
-    //void rotateVector(const double* v1, const double* v2, vnl_matrix<double>& rotation);
-
-
+    vtkIO();
 	vtkDataSet* readDataFile(std::string file);
     vtkPolyData* readFile(std::string file);
-    //vtkDataArray* findFieldData(vtkPolyData* dataSet, std::string propertyName);
     void writeFile(std::string file, vtkDataSet* mesh);
-    //void writeXMLFile(std::string file, vtkPolyData* mesh);
-    
-    //void writeXMLFile(std::string file, vtkStructuredGrid* grid);
-//    vtkDataArray* getPointArray(vtkDataSet* ds);
 
 };
 
